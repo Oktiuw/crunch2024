ff721467b1f5f5903f902c563a521fe4f245f3c6
Aurélien VINCENT <vinau02@gmail.com>
2024-05-15 10:37:06 +0200

    Fin application

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById('launchBot').addEventListener('click', function() {
+    window.open('https://mediafiles.botpress.cloud/166cb92f-c2b8-4937-9670-923b62d2a7c4/webchat/bot.html', '_blank');})
